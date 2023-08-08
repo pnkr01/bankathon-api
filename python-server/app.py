e5765e821432137d2285f032dfa2528a3556cf12
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-08-08 11:03:00 +0530

    Flask api connected and cleaned

--- python-server/app.py
+++ python-server/app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, request
-import openai,os
+import openai
+import os
 from dotenv import load_dotenv
 from provider.JD_Provider import enhance_jd
 from helper.api_response import ApiResponse
@@ -10,24 +11,20 @@ app = Flask(__name__)
 load_dotenv()
 openai.api_key = os.getenv('API_KEY')
 
-##-----------JD Improvement Request---------------##
+## -----------JD Improvement Request---------------##
+
+
 @app.post('/enhance_jd')
 def handle_enhance_jd():
     request_data = request.json
-    requestid = request_data['requestid']
+    request_id = request_data['request_id']
     job_description = request_data['job_description']
-    if not requestid or not job_description:
-        return app.response_class(ApiResponse(400,{"message":"requestid and job_description are required"}))
-    enhanced_jd = enhance_jd( requestid,job_description)
-    return app.response_class(ApiResponse(200,enhanced_jd))
+    if not request_id or not job_description:
+        return app.response_class(ApiResponse(400, {"message": "request_id and job_description are required"}))
+    enhanced_jd = enhance_jd(request_id, job_description)
+    return app.response_class(ApiResponse(200, enhanced_jd))
 
-##-----------JD Improvement Request Ends---------------##
- 
-##-----------Anaylyze Resume Starts---------------##   
-@app.post('/analyze_cv')
-def handle_analyze_cv():
-    request_data = request.json
-    return cv_score(request_data)
-##-----------Anaylyze Resume Starts---------------##   
+
+## -----------Anaylyze Resume Starts---------------##
 if __name__ == '__main__':
     app.run(debug=True)
