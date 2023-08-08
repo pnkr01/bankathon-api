05efbf982a7504ddcbc86197da837b1016550bd2
Pawan Kumar <pawankumar49871@gmail.com>
2023-08-08 10:37:12 +0530

    fix analyze cv

--- python-server/app.py
+++ python-server/app.py
@@ -1,9 +1,11 @@
 
 from flask import Flask, request
 import openai,os
+from utils.response.cv_response import generate_score
 from dotenv import load_dotenv
 from provider.JD_Provider import enhance_jd
 from helper.api_response import ApiResponse
+from routes.cv_score import cv_score
 
 
 app = Flask(__name__)
@@ -25,9 +27,15 @@ def handle_enhance_jd():
  
 ##-----------Anaylyze Resume Starts---------------##   
 @app.post('/analyze_cv')
-def handle_analyze_cv():
+def analyze_cv():
     request_data = request.json
-    return cv_score(request_data)
+    resume_text = request_data['resume_text']
+    job_title = request_data['job_title']
+    skills = request_data['skills']
+    if not resume_text or not job_title or not skills:
+        return app.response_class(ApiResponse(400,{"message":"resume_text, job_title and skills are required"}))
+    score = generate_score(job_title,skills,resume_text)
+    return app.response_class(ApiResponse(200,score))
 ##-----------Anaylyze Resume Starts---------------##   
 if __name__ == '__main__':
     app.run(debug=True)
